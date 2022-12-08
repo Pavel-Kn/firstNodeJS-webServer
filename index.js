7b6f9b3ae3847ba83b5eb114ba1362225bd06e2b
Pavel-Kn <ferrari.inside@gmail.com>
2022-12-08 19:21:27 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const http = require('http')
+const chalk = require('chalk')
+const fs = require('fs/promises')
+const path = require('path')
+const {addNote, getNotes, removeNote} = require('./notes.controller')
+
+const port = 3000;
+const basePath = path.join(__dirname, 'pages')
+
+const server = http.createServer(async (req, res) => {
+    if ( req.method === 'GET') {
+    const content = await fs.readFile(path.join(basePath, 'index.html'))
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        })
+        res.end(content)
+    } else if (req.method === 'POST'){
+        const body = []
+        req.on('data', data => {
+            body.push(Buffer.from(data))
+        })
+        req.on('end', () => {
+            const title = body.toString().split('=')[1].replaceAll('+',' ')
+            addNote(title)
+        })
+        res.end('Post success')
+    }
+})
+
+server.listen(port, () => {
+    console.log(chalk.green(`Server has been started on port: ${port}...`))
+})
