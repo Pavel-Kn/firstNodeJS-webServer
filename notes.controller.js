56fc10210290e2ace3fe34b211200b850dfe0fae
Pavel-Kn <ferrari.inside@gmail.com>
2022-12-09 11:03:31 +0300

    add html interface and buttons

--- notes.controller.js
+++ notes.controller.js
@@ -10,32 +10,40 @@ async function addNote(title) {
         title,
         id: Date.now().toString()
     }
+
     notes.push(note)
 
     await saveNotes(notes)
     console.log(chalk.bgGreen('Note was added!'))
 }
 
+async function getNotes() {
+    const notes = await fs.readFile(notesPath, {encoding: 'utf-8'})
+    return Array.isArray(JSON.parse(notes)) ? JSON.parse(notes) : []
+}
+
 async function saveNotes(notes) {
     await fs.writeFile(notesPath, JSON.stringify(notes))
 }
 
-async function getNotes() {
-    const notes = await fs.readFile(notesPath, {encoding: 'utf-8'})
-    return  Array.isArray(JSON.parse(notes)) ? JSON.parse(notes) : []
-}
-
 async function printNotes() {
     const notes = await getNotes()
+
     console.log(chalk.bgBlue('Here is the list of notes:'))
     notes.forEach(note => {
         console.log(chalk.bgWhite(note.id), chalk.blue(note.title))
     })
 }
+async function log() {
 
+    console.log(chalk.bgBlue('Here is the list of notes:'))
+
+}
 async function removeNote(id) {
     const notes = await getNotes()
+
     const filtered = notes.filter(note => note.id !== id)
+
     await saveNotes(filtered)
     console.log(chalk.red(`Note with id="${id}" has been removed.`))
 }
